95b9c6fc13a3b6c8cbd3297226c64893dedda2f0
manojconcept <manojveerapavu@gmail.com>
2024-03-22 14:42:44 +0530

    removed linked

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.get("/",(req,res)=>{
     res.send(`
     <h3>To create File</h1>
     <a target="_blank" href="https://nodejs-filesystem-jdpe.onrender.com/createFile">https://nodejs-filesystem-jdpe.onrender.com/createFile</a>
-    https://nodejs-filesystem-jdpe.onrender.com/createFile
     <h3>To Retrieve Files</h1>
     <a target="_blank" href="https://nodejs-filesystem-jdpe.onrender.com/retrieveFiles">https://nodejs-filesystem-jdpe.onrender.com/retrieveFiles</a>
     ` )
